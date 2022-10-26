251d22bdd6104aa7e62d97406c8ce1e0c866adf4
Gabriel Cancellieri <cancellieri.gabriel@gmail.com>
2022-10-26 07:11:53 -0300

    ✨ Add Alert Component

--- src/components/common/Alert/Alert.tsx
+++ src/components/common/Alert/Alert.tsx
@@ -1,54 +1,93 @@
-import { Button, Icon, Typography } from "@ccomponents";
+import { Button, Icon, Modal, Typography } from "@ccomponents";
 import { COLOR_WHITE } from "@constants/colors";
 import { BOLD } from "@constants/fonts";
-import { MT_2, MT_3, MT_4 } from "@constants/margins";
+import { MT_2, MT_3 } from "@constants/margins";
 import classNames from "classnames";
 import React from "react";
+import { closeAlert } from "src/context/AlertContext/actions";
+import {
+  AlertDispatchContext,
+  AlertStateContext,
+} from "src/context/AlertContext/AlertContext";
+import { IAlert } from "src/context/AlertContext/types";
 import connect from "src/context/Store/connect";
+import { restorePageOverflowY } from "src/utils/modal";
 import styles from "./alert.module.scss";
-import { ALERT_VARIANTS } from "./constants";
-import { AlertProps, AlertVariant } from "./types";
+import { AlertProps } from "./types";
 
-const Alert = ({ type, variant }: AlertProps) => {
-  const alert: AlertVariant = ALERT_VARIANTS[variant];
+const Alert = ({ alert, dispatch }: AlertProps) => {
+  const handleClose = () => {
+    restorePageOverflowY();
+    dispatch(closeAlert());
+  };
+  if (!alert) {
+    return <></>;
+  }
+  console.log(alert);
   return (
-    <div className={classNames(styles[`${type}Container`], styles.alert)}>
-      {alert.icon && (
-        <Icon
-          size={alert.icon.size}
-          iconId={alert.icon.id}
-          iconDesign={alert.icon.design}
-          color={alert.icon.color}
-        />
-      )}
-      {alert.title && (
-        <Typography
-          className={MT_3}
-          size={alert.title.size}
-          color={alert.title.color}
-          variant={BOLD}
+    <>
+      {alert.type === "urgent" ? (
+        <Modal
+          onCancel={handleClose}
+          width={"500px"}
+          height={"400px"}
+          isActive={Boolean(alert)}
         >
-          {alert.title.text}
-        </Typography>
-      )}
-      {alert.description && (
-        <Typography
-          className={MT_2}
-          size={alert.description.size}
-          color={alert.description.color}
-        >
-          {alert.description.text}
-        </Typography>
-      )}
-      <Button className={styles.confirmButton} variant="secondary">
-        <Typography size={16} variant={BOLD} color={COLOR_WHITE}>
-          OK!
-        </Typography>
-      </Button>
-    </div>
+          <div className={classNames(styles.urgentContainer)}>
+            {alert.variant.icon && (
+              <Icon
+                size={alert.variant.icon.size}
+                iconId={alert.variant.icon.id}
+                iconDesign={alert.variant.icon.design}
+                color={alert.variant.icon.color}
+              />
+            )}
+            {alert.variant.title && (
+              <Typography
+                className={MT_3}
+                size={alert.variant.title.size}
+                color={alert.variant.title.color}
+                variant={BOLD}
+              >
+                {alert.variant.title.text}
+              </Typography>
+            )}
+            {alert.variant.description && (
+              <Typography
+                className={MT_2}
+                size={alert.variant.description.size}
+                color={alert.variant.description.color}
+              >
+                {alert.message}
+              </Typography>
+            )}
+            <Button
+              className={styles.confirmButton}
+              variant="secondary"
+              onClick={handleClose}
+            >
+              <Typography size={16} variant={BOLD} color={COLOR_WHITE}>
+                OK!
+              </Typography>
+            </Button>
+          </div>
+        </Modal>
+      ) : null}
+    </>
   );
 };
 
-const mapStateToProps: any = [];
+const mapStateToProps: any = [
+  {
+    context: AlertStateContext,
+    mapStateToProps: ({ currentAlert }: { currentAlert: IAlert }) => ({
+      alert: currentAlert,
+    }),
+  },
+];
 
-export default Alert;
+const mapDispatchToProps: any = {
+  dispatch: AlertDispatchContext,
+};
+
+export default connect({ mapStateToProps, mapDispatchToProps })(Alert);
